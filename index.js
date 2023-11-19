de8aaa6ca80116c28d90e87ed0933b85ba759c04
vno-thvishnu <vinothvishnu12@gmail.com>
2023-11-19 17:46:06 +0530

    push

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
   // origin: "http://localhost:3000",
-  origin: "https://recipe-application-frontend.vercel.app/",
+  // origin: "https://recipe-application-frontend.vercel.app/",
+  origin:"*"
 }
 ));
 app.use(express.json());
