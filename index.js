f3e99fb8651dda521719ab65866ae13e112bd389
vno-thvishnu <vinothvishnu12@gmail.com>
2023-11-19 16:14:28 +0530

    push

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const UserRoute = require ("./Routes/UserRoute.js")
 //Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  // origin: "https://b39-ii-capstone-foodtokengenerator-frontend.vercel.app",
+}));
 app.use(express.json());
 
 // const PORT = process.env.PORT;
